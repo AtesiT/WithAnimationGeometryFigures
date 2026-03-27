215d68f9193492b8e2a1aed75840c8b376b220f7
AtesiT <atesit@mail.ru>
2026-03-27 23:49:02 +0300

    ADD: background (sky and grass)

--- WithAnimationGeometryFigures/ContentView.swift
+++ WithAnimationGeometryFigures/ContentView.swift
@@ -2,10 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
+        ZStack {
+            VStack(spacing: 0) {
+                Color(
+                    red: 135/255,
+                    green: 205/255,
+                    blue: 255/255,
+                )
+                Color(
+                    red: 0/255,
+                    green: 150/255,
+                    blue: 0/255,
+                )
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
