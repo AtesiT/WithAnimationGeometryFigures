755e50f8ccddd1bce0ca36542604222469800261
AtesiT <atesit@mail.ru>
2026-03-29 11:23:58 +0300

    FIX: TreeView (made spacing, width and height for every frame, replace addLines on addRect)

--- WithAnimationGeometryFigures/Views/TreeView.swift
+++ WithAnimationGeometryFigures/Views/TreeView.swift
@@ -2,34 +2,30 @@ import SwiftUI
 
 struct TreeView: View {
     var body: some View {
-        ForEach(0..<3) { iteration in
-            Path { path in
-                path.move(to: CGPoint(x: 25, y: 0))
-                path.addLine(to: CGPoint(x: 50, y: 50))
-                path.addLine(to: CGPoint(x: 0, y: 50))
-                path.closeSubpath()
+        //  Spacing -5 для того, чтобы убрать "пустые зоны" между зелеными треугольниками
+        VStack(spacing: -5) {
+            ForEach(0..<3) { iteration in
+                Path { path in
+                    path.move(to: CGPoint(x: 25, y: 0))
+                    path.addLine(to: CGPoint(x: 50, y: 50))
+                    path.addLine(to: CGPoint(x: 0, y: 50))
+                    path.closeSubpath()
+                }
+                .fill (Color(red: 50/255, green: 110/255, blue: 50/255))
+                //  Указываем размер для каждого элемента
+                .frame(width: 50, height: 50)
             }
-            .fill (
-                Color(
-                    red: 50/255,
-                    green: 110/255,
-                    blue: 50/255
-                )
-            )
+            //  Используем addRect, чтобы добавить ствол дерева в виде прямоугольника
+            Path { path in
+                path.addRect(CGRect(x: 15, y: 0, width: 20, height: 40))
+            }
+            .fill(Color(.brown))
+            .frame(width: 50, height: 40)
         }
-        Path { path in
-            path.move(to: CGPoint(x: 15, y: 0))
-            path.addLine(to: CGPoint(x: 35, y: 0))
-            path.addLine(to: CGPoint(x: 35, y: 50))
-            path.addLine(to: CGPoint(x: 15, y: 50))
-        }
-        .fill(
-            Color(.brown)
-        )
     }
 }
 
 #Preview {
     TreeView()
-        .frame(width: 50, height: 50)
+        .frame(width: 100, height: 100)
 }
